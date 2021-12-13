8d76ea4a14835b80ffd0de4232c09bae408b1a68
Tim Work <timothy.jarred.frame@gmail.com>
2021-12-13 16:56:40 +0000

    1st styled component test on h1 in Home.js

--- src/components/static/Home.js
+++ src/components/static/Home.js
@@ -1,9 +1,19 @@
 import React from "react";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  font-family: helvetica;
+`
 
 function Home(){
   return(
     <div>
+      <Title>
       <h1>Home Page</h1>
+        </Title>
+      
     </div>
   )
 }
